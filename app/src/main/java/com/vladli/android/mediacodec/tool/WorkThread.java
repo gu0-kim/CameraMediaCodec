9869bcee610f7314081aff792409f26e8b962808
developergu <developergu@126.com>
2018-12-18 16:48:26 +0800

    step1

--- app/src/main/java/com/vladli/android/mediacodec/tool/WorkThread.java
+++ app/src/main/java/com/vladli/android/mediacodec/tool/WorkThread.java
@@ -3,14 +3,19 @@ package com.vladli.android.mediacodec.tool;
 import java.io.IOException;
 
 public class WorkThread extends Thread {
+  // video output dimension
+  static final int OUTPUT_WIDTH = 640;
+  static final int OUTPUT_HEIGHT = 480;
+  SurfaceTextureManager mStManager;
 
   @Override
   public void run() {
-    SurfaceTextureManager st = new SurfaceTextureManager();
+    mStManager = new SurfaceTextureManager();
     CameraDevice cameraDevice = new CameraDevice();
     cameraDevice.prepareCamera(OUTPUT_WIDTH, OUTPUT_HEIGHT);
     try {
-      cameraDevice.getCamera().setPreviewTexture(st.getSurfaceTexture());
+      cameraDevice.getCamera().setPreviewTexture(mStManager.getSurfaceTexture());
+      cameraDevice.getCamera().startPreview();
     } catch (IOException e) {
       e.printStackTrace();
     }
